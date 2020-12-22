ca9fe2a288a069abe2f41e5298dd268504aef78a
Hòa Lê Thị Trúc <17520491@ms.uit.edu.vn>
2020-12-22 15:07:14 +0700

    migration database

---
+++ shopOnl.Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using shopOnl.Data.Entities;
+using shopOnl.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shopOnl.Data.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+
+            builder.Property(x => x.Status).HasDefaultValue(Status.Active);
+        }
+
+     
+    }
+}
