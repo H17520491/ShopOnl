d91b8b2b5a2ccec6e94918287099bcbbe779e993
Hòa Lê Thị Trúc <17520491@ms.uit.edu.vn>
2020-12-31 13:52:13 +0700

    application layer

---
+++ shopOnl.Application/Catalog/Products/Dtos/Manage/GetProductPagingRequest.cs
@@ -0,0 +1,14 @@
+﻿using shopOnl.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shopOnl.Application.Catalog.Products.Dtos.Manage
+{
+    public class GetProductPagingRequest : PagingRequestBase
+    {
+        public string Keyword { get; set; }
+        public List<int> CategoryIds { get; set; }
+
+    }
+}
---
+++ shopOnl.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,14 @@
+﻿using shopOnl.Application.Catalog.Products.Dtos;
+using shopOnl.Application.Catalog.Products.Dtos.Public;
+using shopOnl.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shopOnl.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        PageResulf<ProductViewModel> GetAllByCategoryId(GetProductPagingRequest request);
+    }
+}
